9293c7effc3e23d669a2d4bf8224a0bf20a3f031
Daniel Bogden <danielbogden@u.boisestate.edu>
2020-12-09 21:47:14 -0700

    First real push

--- FinalProject/systick/src/main.c
+++ FinalProject/systick/src/main.c
@@ -4,6 +4,11 @@
 
 #include "adc.h"
 #include "seg7.h"
+/* #include <Servo.h>  This is included in the servo online that you linked, used for writing the movement to the servo
+*                      and reading the current angle.
+*                      the function write() takes in a degree in which you want it to adjust to.
+*                      the read() function will return a degree
+*/
 
 //Function Declarations
 void gpio_d_init(void);
@@ -24,14 +29,22 @@ unsigned int counter = 1;
 
 unsigned int switch1 = 0; //switch to change from auto (0) to manual (0)
 
+void servoSetup()
+{
+  // xAxisServo.attach(9);  Only works with pins 9 and 10, attaches the servo
+  // yAxisServo.attach(10);  Only works with pins 9 and 10, attaches the servo
+}
+
 int main()
 {
 
-  adc_init();  // Initialize the ADC hardware
-  seg7_init(); // Initialize the 7 segment display
-  adc_start(); // Start adc
+  // Create a server object Servo myservo
+  servoSetup(); // Sets up and attaches 2 serves on pins 9 and 10 -- Defined above
+  adc_init();   // Initialize the ADC hardware
+  seg7_init();  // Initialize the 7 segment display
+  adc_start();  // Start adc
 
-  /* set all the seven seg displays in order*/
+  /* set all the seven seg displays in order [0-9]*/
   int sevenSeg[11] = {0x40, 0x79, 0x24, 0x30, 0x19, 0x12, 0x2, 0x78, 0x0, 0x18};
 
   //set system clock
@@ -50,10 +63,13 @@ int main()
 
   while (!adc_done())
   {
+    // This will wait for adc to finish
     continue;
   }
 
-  //loop forever
+  /* Once the ADC function finishes this will be the while loop that makes all the calls
+  * and keeps the program continuously running
+  */
   while (1)
   {
     //get the value of switch 1 using the gpio_c_get function
@@ -64,8 +80,35 @@ int main()
     uint32_t val = adc_get();
 
     /*
-  *   
-  * */
+    * This will be the section that writes to the two servos. It will write no matter which mode.
+    * The only difference will be which servo is active.  The options are Left, Right, or Both
+    * 
+    * The volt_to_degree() is a custom function we wrote down below to convert the ADC count to degrees
+    * 
+    * PSEUDO Code
+    * if (switch for x axis is on) {
+    *     xAxisServo.write(volt_to_deg(val));
+    * 
+    *   }
+    * 
+    * if (switch for y axis is on) {
+    *   yAxisServo.write(volt_to_deg(val));
+    * }
+    * 
+    * if (switch for both is on) {
+    *   xAxisServo.write(volt_to_deg(val));
+    *   yAxisServo.write(volt_to_deg(val));
+    * 
+    */
+
+    /*
+    *   The values for the voltage will be converted to a step in degrees
+    *   and will be right to the four rightmost LEDS 0 and 1 will be the x axis, 2 and 3 will be the y axis
+    *   this will be written in every pass of the while loop with a seg7_put(<LED num in hex>, <hexTo7 value from array> )
+    *   These values with be calculated with a mod to isolate the tens and ones digit.
+    *   Each axis will use 2 LEDS to right 00 to 90
+    *   These values will all be written in the code above.
+    */
 
     continue;
   }
@@ -76,18 +119,9 @@ int main()
 void SysTick_Handler(void) //ISR - SysTick Interrupt Service Routine
 
 {
-  //toggle Port D - LED 15
-  // *pLEDs ^= (1 << 15);
 
-  /* Light up LEDs 0 through 15, once all on turn them off and repeat. */
-  if (counter > 15)
-  {
-    counter = 0;
-    *pLEDs = 0;
-  }
-  else
-  {
-    *pLEDs |= (1 << counter);
-    counter++;
-  }
+  /*
+  *  This next part will control
+  * 
+  */
 }

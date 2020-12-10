d7455bfac3fe6a762cdeb5e8b88afd0287ddc4c1
Daniel Bogden <danielbogden@u.boisestate.edu>
2020-12-09 20:12:04 -0700

    Daniel's code... and stuff

--- FinalProject/systick/src/main.c
+++ FinalProject/systick/src/main.c
@@ -2,6 +2,9 @@
 * Author: Bethany Hull
 */
 
+#include "adc.h"
+#include "seg7.h"
+
 //Function Declarations
 void gpio_d_init(void);
 unsigned int SysTick_Config(unsigned int ticks);
@@ -10,7 +13,9 @@ unsigned int gpio_c_init(void);
 // also add gpio get and put functions to write to and from switches
 
 //# Defines
-#define reload_value (1 << 23) - 1 /* System Timer reload value */
+
+#define reload_value (1 << 23) - 1 /* Reload value is 20x10^-6 / 5.96 * 10^-9  */
+
 #define LEDs_ODR_Base (0x40020C14) /* LEDs Port D ODR Address */
 
 unsigned int *pLEDs = (unsigned int *)LEDs_ODR_Base; /* Create pointer to Port D - ODR Reg */
@@ -19,9 +24,15 @@ unsigned int counter = 1;
 
 unsigned int switch1 = 0; //switch to change from auto (0) to manual (0)
 
-
 int main()
 {
+
+  adc_init();  // Initialize the ADC hardware
+  seg7_init(); // Initialize the 7 segment display
+  adc_start(); // Start adc
+
+  /* set all the seven seg displays in order*/
+  int sevenSeg[11] = {0x40, 0x79, 0x24, 0x30, 0x19, 0x12, 0x2, 0x78, 0x0, 0x18};
 
   //set system clock
   SetSysClock();
@@ -33,18 +44,30 @@ int main()
   gpio_c_init();
 
   //configure SysTick System Timer
+
+  /* reload value = 20x10^-6 / 5.96 * 10^-9 */
   SysTick_Config(reload_value);
+
+  while (!adc_done())
+  {
+    continue;
+  }
 
   //loop forever
   while (1)
   {
-    //get the value of switch 1 using the gpio_c_get function 
+    //get the value of switch 1 using the gpio_c_get function
     //isolate the bit 0 to get the value of switch 1 and set
     //it to the switch 1 variable
-    
 
-    
-    
+    /* This will continually grab the value of the voltage by the user if it is manual mode */
+    uint32_t val = adc_get();
+
+    /*
+  *   
+  * */
+
+    continue;
   }
 
   return 0;
